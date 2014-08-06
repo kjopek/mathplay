cb66392fea74913b75bd1e09776a9fc7c5907814
kjopek <kjopek@gmail.com>
2014-08-06 18:51:47 +0200

    working openmp version

--- chol_sparse.c
+++ chol_sparse.c
@@ -206,7 +206,7 @@ void chol(sm *mat, sm *mat_col)
             return ;
         }
         mat->x[p] = sqrt(x);
-        //#pragma omp parallel for schedule(dynamic, CHUNKSIZE)
+        #pragma omp parallel for private(q,y) schedule(dynamic, CHUNKSIZE)
         for (j=mat_col->ptr[i]; j<mat_col->ptr[i+1]; ++j) {
 
             if (mat_col->ind[j] <= i) continue;
@@ -298,14 +298,14 @@ int main(int argc, char ** argv)
 
     gettimeofday(&t1, NULL);
     chol(matrix, matrix_col);
-    //debug_matrix(matrix);
+    debug_matrix(matrix);
     gettimeofday(&t2, NULL);
     printf("SOLVE time: ");
     print_time(&t1, &t2);
 
     for (i=0; i<matrix->n; ++i) {
         for (j=matrix->ptr[i]; j<matrix->ptr[i+1]; ++j) {
-            printf("%d %d %e\n", i, matrix->ind[j], matrix->x[j]);
+            printf("%d %d %E\n", i, matrix->ind[j], matrix->x[j]);
         }
     }
 
